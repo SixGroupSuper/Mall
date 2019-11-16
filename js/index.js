1fe7f193a23d486f79868184c657fc10fe5ff9ae
Andoped <1196341787@qq.com>
2019-11-16 12:10:59 +0800

    js显示更多

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 var txt = '{ "sites" : [' +'{ "name":"官方奢品直营  国内直邮  包邮包税" ,"yen":"1199" ,"benefit":"买贵赔" ,"prices":"90" , "content":"当季新品正品保证" , "content2":"预售 Andre Maurice 黑色山羊绒长" , "url":"https://gju4.alicdn.com/tps/i3/2670830951/O1CN01KAJa1u1ItbDuuffZ4_!!0-item_pic.jpg_400x400Q90.jpg_.webp" }]}';
 var obj = eval ("(" + txt + ")");
 
-
-
 document.getElementsByClassName("desc")[1].innerHTML=obj.sites[0].name
 document.getElementsByClassName("item-pic")[1].src=obj.sites[0].url
 document.getElementsByClassName("price-point")[1].innerHTML+=obj.sites[0].content
@@ -12,11 +10,7 @@ document.getElementsByClassName("benefit")[1].innerHTML=obj.sites[0].benefit
 document.getElementsByClassName("prices")[0].innerHTML+=obj.sites[0].prices
 console.log();
 
-
-
-
-
-var xmlhttp=new XMLHttpRequest();
+/* var xmlhttp=new XMLHttpRequest();
 xmlhttp.onreadystatechange=function(){
     if(this.readyState==4&&this.status==200){
         myobj=JSON.parse(this.responseText);
@@ -25,5 +19,39 @@ xmlhttp.onreadystatechange=function(){
     }
 };
 xmlhttp.open("post","./json/index.json",true);
-xmlhttp.send();
+xmlhttp.send(); */
+
+
+function myFunction(){
+    for (var i = 0; i < 3; i++) {
+    var li= document.getElementsByClassName("item-small-v3")[i];
+    var cln=li.cloneNode(true);
+    document.getElementsByClassName("clearfix")[1].appendChild(cln);
+    console.log(li);
+    }
+}
+
+function newFunction(){
+    for (var t = 0; t < 2; t++) {
+    var t1= document.getElementsByClassName("item-small-v2")[t];
+    var t2=t1.cloneNode(true);
+    document.getElementsByClassName("clearfix")[2].appendChild(t2);
+    console.log(t1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
